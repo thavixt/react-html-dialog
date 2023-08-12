66b74c7ac4a68c9bd43ddef8ddfefb93ebc49f95
thavixt <komlosi.peti@gmail.com>
2023-08-12 16:25:51 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import Dialog from './components/Dialog';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div className="card">
+        <button onClick={() => setDialogOpen(true)}>Open dialog</button>
+      </div>
+      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+        <p>Dialog text here...</p>
+        <input name="username" type="text" />
+        <button>OK</button>
+      </Dialog>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Dialog.tsx
@@ -0,0 +1,35 @@
+import { useRef, FormEventHandler, PropsWithChildren } from 'react';
+import './Dialog.css';
+
+interface DialogProps<T = boolean> {
+  open?: boolean;
+  onClose: () => void;
+  onSubmit?: FormEventHandler<HTMLDialogElement>;
+}
+
+export default function Dialog({
+  children,
+  open,
+  onClose,
+  onSubmit,
+}: PropsWithChildren<DialogProps>) {
+  const ref = useRef<HTMLDialogElement>(null);
+  //console.log({ open });
+
+  if (!open) return null;
+
+  return (
+    <dialog
+      ref={ref}
+      open={open}
+      onClose={onClose}
+      onCancel={onClose}
+      onSubmit={onSubmit}
+      className="Dialog__container"
+    >
+      <div className="Dialog">
+        <form method="dialog">{children}</form>
+      </div>
+    </dialog>
+  );
+}
