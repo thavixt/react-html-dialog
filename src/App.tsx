f56380e769dfea1329df738a14b4602f0147de89
thavixt <komlosi.peti@gmail.com>
2023-08-14 17:17:31 +0200

    more dialog input types

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import Dialog from './components/Dialog';
 function App() {
   const [count, setCount] = useState(0);
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogOpen2, setDialogOpen2] = useState(false);
+  const [dialogOpen3, setDialogOpen3] = useState(false);
+  const [dialogOpen4, setDialogOpen4] = useState(false);
+
+  const [lastAnswer, setLastAnswer] = useState<string | null>(null);
 
   return (
     <>
@@ -19,24 +24,91 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <div className="card">
-        <button onClick={() => setDialogOpen(true)}>Open dialog</button>
-      </div>
-      <Dialog
-        open={dialogOpen}
-        onClose={() => setDialogOpen(false)}
-        onSubmit={console.log}
-      >
-        <p>Choose a username:</p>
-        <input name="username" type="text" />
-        <button value="cancel">Cancel</button>
-        <button>Confirm</button>
-      </Dialog>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
+
+      {lastAnswer !== null ? (
+        <p>
+          Your last answer was <code>{lastAnswer.toString()}</code>
+        </p>
+      ) : null}
+
+      <div className="card">
+        <button onClick={() => setDialogOpen(true)}>Open text dialog</button>
+      </div>
+      <Dialog
+        open={dialogOpen}
+        onClose={() => setDialogOpen(false)}
+        onSubmit={setLastAnswer}
+        confirmText="Confirm"
+        cancelText="Cancel"
+      >
+        <p>Choose a username:</p>
+        <input name="dialogInput" type="text" />
+      </Dialog>
+
+      <div className="card">
+        <button onClick={() => setDialogOpen2(true)}>
+          Open checkbox dialog
+        </button>
+      </div>
+      <Dialog
+        open={dialogOpen2}
+        onClose={() => setDialogOpen2(false)}
+        onSubmit={setLastAnswer}
+        confirmText="Let's see"
+        cancelText="Nah..."
+      >
+        <p>Answer this honestly:</p>
+        <label htmlFor="dialogInput">Is this working?</label>
+        <input type="checkbox" name="dialogInput" />
+      </Dialog>
+
+      <div className="card">
+        <button onClick={() => setDialogOpen3(true)}>Open radio dialog</button>
+      </div>
+      <Dialog
+        open={dialogOpen3}
+        onClose={() => setDialogOpen3(false)}
+        onSubmit={setLastAnswer}
+        confirmText="Let's see"
+        cancelText="Nah..."
+      >
+        <p>Best part:</p>
+        <input type="radio" id="html" name="dialogInput" value="HTML" />
+        <label htmlFor="html">HTML</label>
+        <br />
+        <input type="radio" id="css" name="dialogInput" value="CSS" />
+        <label htmlFor="css">CSS</label>
+        <br />
+        <input
+          type="radio"
+          id="javascript"
+          name="dialogInput"
+          value="JavaScript"
+        />
+        <label htmlFor="javascript">JavaScript</label>
+      </Dialog>
+
+      <div className="card">
+        <button onClick={() => setDialogOpen4(true)}>
+          Open datetime-local dialog
+        </button>
+      </div>
+      <Dialog
+        open={dialogOpen4}
+        onClose={() => setDialogOpen4(false)}
+        onSubmit={setLastAnswer}
+        confirmText="Let's see"
+        cancelText="Nah..."
+      >
+        <p>Best time in recent memory was:</p>
+        <input type="datetime-local" name="dialogInput" />
+      </Dialog>
     </>
   );
 }
--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,34 +1,80 @@
-import { useRef, FormEventHandler, PropsWithChildren } from 'react';
+import { useCallback, useRef, PropsWithChildren } from 'react';
 import './Dialog.css';
 
-interface DialogProps {
+const INPUT_NAME = 'dialogInput';
+
+interface DialogProps<T> {
+  cancelText?: string | boolean;
+  confirmText?: string | boolean;
+  onClose?: () => void | Promise<void>;
+  onSubmit: (value: T) => void | Promise<void>;
   open?: boolean;
-  onClose: () => void;
-  onSubmit?: FormEventHandler<HTMLDialogElement>;
 }
 
-export default function Dialog({
+export default function Dialog<T = string | number | boolean>({
   children,
-  open,
+  confirmText = 'Confirm',
+  cancelText = 'Cancel',
   onClose,
   onSubmit,
-}: PropsWithChildren<DialogProps>) {
-  const ref = useRef<HTMLDialogElement>(null);
-  //console.log({ open });
+  open,
+}: PropsWithChildren<DialogProps<T>>) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const onDialogSubmit = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+      if (!dialogRef.current) {
+        return;
+      }
+
+      const formInput = dialogRef.current.querySelector(
+        `[name=${INPUT_NAME}]`
+      ) as HTMLInputElement; // Get the first input element in the dialog
+
+      console.log('Dialog input type is', formInput.type);
+      switch (formInput.type) {
+        case 'radio':
+          // It's radio elements
+          const value = document.getElementsByName(INPUT_NAME);
+          for (const element of value) {
+            const radio = element as HTMLInputElement;
+            if (radio.checked) {
+              onSubmit(radio.value as T);
+            }
+          }
+          break;
+        case 'checkbox':
+          onSubmit(formInput.checked as T);
+          break;
+        default:
+          onSubmit(formInput.value as T);
+          break;
+      }
+      //await new Promise((res) => setTimeout(res, 250)); // Loading animation?
+      await onClose?.();
+    },
+    []
+  );
 
   if (!open) return null;
 
   return (
-    <dialog
-      ref={ref}
-      open={open}
-      onClose={onClose}
-      onCancel={onClose}
-      onSubmit={onSubmit}
-      className="Dialog__container"
-    >
+    <dialog ref={dialogRef} open={open} className="Dialog__container">
       <div className="Dialog">
         <form method="dialog">{children}</form>
+        <div className="Dialog__buttonContainer">
+          {confirmText && (
+            <button type="button" onClick={onDialogSubmit}>
+              {confirmText}
+            </button>
+          )}
+          {cancelText && (
+            <button type="button" onClick={onClose}>
+              {cancelText}
+            </button>
+          )}
+        </div>
       </div>
     </dialog>
   );
